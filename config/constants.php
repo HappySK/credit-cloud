33b2d8dfdfebe4ceb29aeed75c053c88b4660f16
HappySK <shravanyoserene1729@gmail.com>
2020-10-23 10:03:03 +0530

    Revereted changes to constants.php

--- config/constants.php
+++ config/constants.php
@@ -2,5 +2,4 @@
   defined('TITLE') || define('TITLE','Credit Repair Cloud');
   defined('LINK') || define('LINK','sravan.sowlab.website/credit');
   defined('MAIN_PATH') || define('MAIN_PATH',$_SERVER['DOCUMENT_ROOT'].'/credit');
-  defined('ASSETS_PATH') || define('MAIN_PATH',$_SERVER['DOCUMENT_ROOT'].'/credit/assets');
 ?>
