74faff0ccc13863e0f3e246ee028dd1ab9693181
HappySK <shravanyoserene1729@gmail.com>
2020-10-31 13:38:10 +0530

    Changes in file systems

--- super-admin/update-role.php
+++ super-admin/update-role.php
@@ -1,8 +1,8 @@
 <?php
   if(isset($_POST) && !empty($_POST))
   {
-    require '../server-side/class/dbconnect.php';
-    require '../server-side/class/super-admin.php';
+    require '../config/constants.php';
+    require CLASS_PATH.'/super-admin.php';
     $admin->update_role($_POST['select-role'],$_POST['select-user']);
     header('Location:super-admin-dashboard');
   }
