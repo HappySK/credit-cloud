3861564d3d3991a9d3524c0bf7a0359bc3d76d39
HappySK <shravanyoserene1729@gmail.com>
2020-11-04 19:14:41 +0530

    Responsible for rendering data in the datatable

--- assets/js/my-clients/clients-list.js
+++ assets/js/my-clients/clients-list.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
-  $('#clients-list-datatable').DataTable({
-    paging:false,
-    info : false,
-    ajax : 'assets/datatables/clients-list-datatable.php',
-    columns : [
-      {data : 's_no'},
-      {data : 'name'},
-      {data : 'assigned_to'},
-      {data : 'referred_by'},
-      {data : 'date_created'},
-      {data : 'date_of_start'},
-      {data : 'date_login'},
-      {data : 'status'},
-      {data : 'action'}
-    ]
-  });
-})
+$(document).ready(function () {
+	$("#clients-list-datatable").DataTable({
+		paging: false,
+		info: false,
+		ajax: "assets/datatables/clients-list-datatable.php",
+		columns: [
+			{ data: "s_no" },
+			{ data: "name" },
+			{ data: "assigned_to" },
+			{ data: "referred_by" },
+			{ data: "date_created" },
+			{ data: "date_of_start" },
+			{ data: "date_login" },
+			{ data: "status" },
+			{ data: "action" },
+		],
+	});
+});
