4381074da93ecfa9b5f0b28bab22c1dfbe1b1ad7
HappySK <shravanyoserene1729@gmail.com>
2020-11-16 22:33:35 +0530

    Added Dynamic Rendering and Form validation for the form

--- assets/js/my-company/my-team-members.js
+++ assets/js/my-company/my-team-members.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+	// For Creating alert for team member deletion
 	$(".sweet-alert").click(function (e) {
 		Swal.fire({
 			title: "Are you sure?",
@@ -23,6 +25,7 @@ $(document).ready(function () {
 		});
 	});
 
+	// SweetAlert for enabling and disabling team chat
 	$("#enable-disable-button").click(function () {
 		if ($(this).text() == "Enable Team Chat Now") {
 			Swal.fire(
@@ -39,5 +42,138 @@ $(document).ready(function () {
 			);
 			$(this).text("Enable Team Chat Now");
 		}
-  });
+	});
+
+	// For rendering roles from the database table
+	$.getJSON("server-side/class/my-company/roles.php", function (roles, status) {
+		$.each(roles, function (index, value) {
+			var select_role = document.getElementById("select-role");
+			var option = document.createElement("option");
+			option.value = value.roles;
+			option.text = value.roles;
+			select_role.appendChild(option);
+		});
+	});
+
+	// Form validation for add-team-members form
+	FormValidation.formValidation(
+		document.getElementById("add-team-members-form"),
+		{
+			fields: {
+				first_name: {
+					validators: {
+						notEmpty: {
+							message: "Company Name is required",
+						},
+					},
+				},
+				user_id: {
+					validators: {
+						notEmpty: {
+							message: "Company Name is required",
+						},
+					},
+				},
+				last_name: {
+					validators: {
+						notEmpty: {
+							message: "Company Name is required",
+						},
+					},
+				},
+				password: {
+					validators: {
+						notEmpty: {
+							message: "Company Name is required",
+						},
+					},
+				},
+				gender: {
+					validators: {
+						notEmpty: {
+							message: "Company Name is required",
+						},
+					},
+				},
+				member_email: {
+					validators: {
+						notEmpty: {
+							message: "Company Name is required",
+						},
+					},
+				},
+				phone: {
+					validators: {
+						notEmpty: {
+							message: "Company Name is required",
+						},
+					},
+				},
+				ext: {
+					validators: {
+						notEmpty: {
+							message: "Company Name is required",
+						},
+					},
+				},
+				mobile: {
+					validators: {
+						notEmpty: {
+							message: "Company Name is required",
+						},
+					},
+				},
+				fax: {
+					validators: {
+						notEmpty: {
+							message: "Company Name is required",
+						},
+					},
+				},
+				title: {
+					validators: {
+						notEmpty: {
+							message: "Company Name is required",
+						},
+					},
+				},
+				member_address: {
+					validators: {
+						notEmpty: {
+							message: "Company Name is required",
+						},
+					},
+				},
+				select_role: {
+					validators: {
+						notEmpty: {
+							message: "Company Name is required",
+						},
+					},
+				},
+				photo_file: {
+					validators: {
+						notEmpty: {
+							message: "Company Name is required",
+						},
+					},
+				},
+				
+			},
+			plugins: {
+				trigger: new FormValidation.plugins.Trigger(),
+				// Bootstrap Framework Integration
+				bootstrap: new FormValidation.plugins.Bootstrap(),
+				// Validate fields when clicking the Submit button
+				submitButton: new FormValidation.plugins.SubmitButton(),
+				// Submit the form when all fields are valid
+				defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
+			},
+		}
+	);
+
+	//Fetching the team-members-details
+	$.get('server-side/class/my-company/add-team-member.php',{action : 'get_team_members'},function(data,status){
+		console.log(status)
+	})
 });
