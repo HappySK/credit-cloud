c6aff88b790daae38bd56d0b8a6d8e02916b74d2
HappySK <shravanyoserene1729@gmail.com>
2020-11-18 14:55:04 +0530

    The function is added so that we can fetch the details of the team-members

--- server-side/functions/functions.php
+++ server-side/functions/functions.php
@@ -1,7 +1,7 @@
 <?php
-  require CLASS_PATH.'/dbconnect.php';
+  // require CLASS_PATH.'/dbconnect.php';
   $connection = new dbconnect($config->DB_CREDENTIALS);
-  session_start();
+  // session_start();
   function get_client($id,$connection)
   {
     try
@@ -10,6 +10,22 @@
       $stmt = $connection->conn->prepare($sql);
       $stmt->execute([$id]);
       return $stmt->fetchObject();
+    }
+    catch(SQLException $e)
+    {
+      echo $e->getMessage();
+    }
+  }
+
+  // Included in my-company/team-member.php
+  function get_team_members($connection)
+  {
+    try
+    {
+      $sql = "SELECT * FROM `mycompany__team_members` WHERE `user_id` = ?";
+      $stmt = $connection->conn->prepare($sql);
+      $stmt->execute([$_SESSION['user_id']]);
+      return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     catch(SQLException $e)
     {
