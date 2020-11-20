172fb4b97247393d6cd3b7b853630a9d01758f94
HappySK <shravanyoserene1729@gmail.com>
2020-11-20 22:50:30 +0530

    The Datatable which is rendered from the datatable in the form of AJAX

--- assets/js/my-company/roles-and-permissions.js
+++ assets/js/my-company/roles-and-permissions.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
   $('#roles-table').DataTable({
     searching:false,
     info:false,
-    paging:false
+    paging:false,
+    ajax : 'assets/datatables/my-company/roles-datatable.php',
+    columns: [
+			{ data: "roles" },
+			{ data: "actions" }
+		],
   });
 })
