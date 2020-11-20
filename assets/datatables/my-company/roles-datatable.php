88d70f918ba341480b42920965d9dd443213c51e
HappySK <shravanyoserene1729@gmail.com>
2020-11-20 22:49:50 +0530

    The Roles from the database is rendered into the datatable

--- assets/datatables/my-company/roles-datatable.php
+++ assets/datatables/my-company/roles-datatable.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  require '../../config/config.php';
+  require '../../../config/config.php';
   require CLASS_PATH.'/dbconnect.php';
   class roles_datatable extends dbconnect
   {
@@ -9,11 +9,36 @@
       parent :: __construct($config);
     }
 
+    function add_roles($role)
+    {
+      try
+      {
+        $sql = "INSERT INTO `mycompany__roles`(`roles`) VALUES (?)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute($role);
+      }
+      catch(SQLException $e)
+      {
+        echo $e->getMessage();
+      }
+    }
+
     function get_roles()
     {
       try
       {
-
+        $sql = "SELECT * FROM `mycompany__roles`";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $data = array();
+        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        foreach($records as $record)
+        {
+          $row['roles'] = $record['roles'];
+          $row['actions'] = '<a href="my-company/roles-and-permissions/manage-permissions?action=view&role_id='.$record['s_no'].'">View Permissions</a> | <a href="my-company/roles-and-permissions/manage-permissions?action=manage&role_id='.$record['s_no'].'">Manage Permissions</a>';
+          array_push($data,$row);
+        }
+      echo json_encode(array('data' => $data));
       }
       catch(SQLException $e)
       { 
@@ -21,4 +46,8 @@
       }
     }
   }
+
+  $role = new roles_datatable($config->DB_CREDENTIALS);
+  $role->get_roles();
+
 ?>
