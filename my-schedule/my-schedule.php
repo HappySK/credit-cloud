1ff20d2a4998ab18d107e08834ad536a5c4d2b44
HappySK <shravanyoserene1729@gmail.com>
2020-11-27 20:35:10 +0530

    Minor Bug Fixes in my-schedule tab

--- my-schedule/my-schedule.php
+++ my-schedule/my-schedule.php
@@ -1,3 +1,12 @@
+<?php require '../config/config.php'?>
+<?php session_start(); ?>
+<?php
+  require CLASS_PATH.'/user.php';
+  if(isset($_SESSION['id']))
+  {
+    $user_details = $user->get_data($_SESSION['id']);     
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <!--begin::Head-->
@@ -5,15 +14,6 @@
 <head>
   <base href="../">
   <meta charset="utf-8" />
-  <?php require '../config/config.php'?>
-  <?php session_start(); ?>
-  <?php
-    require CLASS_PATH.'/user.php';
-    if(isset($_SESSION['id']))
-    {
-      $user_details = $user->get_data($_SESSION['id']);     
-    }
-  ?>
   <title><?= TITLE ?> | My Schedule</title>
   <meta name="description" content="Page with empty content" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
